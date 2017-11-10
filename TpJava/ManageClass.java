62e3fd18aeda87683f40b998708308ddf3b6835d
root <root@DESKTOP-4S06HK7.localdomain>
2017-11-10 15:12:24 +0000

    Student work

---
+++ TpJava/ManageClass.java
@@ -0,0 +1,54 @@
+
+import java.util.*;
+import myPackage.Class_StringVector;
+
+
+public class ManageClass
+{
+	public static void main(String [] arg)
+	{
+		byte choice = 0;
+		Scanner scan = new Scanner(System.in);
+		Class_StringVector V = new Class_StringVector();
+		
+		do
+		{
+			System.out.println("1. Add a string");
+			System.out.println("2. Print elements");
+			System.out.println("3. Clear the container");
+			System.out.println("4. Sort the container");
+			System.out.println("5. Print the number of duplication(s)");
+			System.out.println("6. Remove duplication(s)");
+			System.out.println("7. Quit");
+			System.out.print(">");
+			choice = scan.nextByte();
+			
+			switch(choice)
+			{
+				case 1 :
+					V.fillVector();
+					break;
+				case 2 :
+					V.printVector();
+					break;
+				case 3 :
+					V.clearVector();
+				case 4 :
+					V.sortVector();
+					break;
+				case 5 :
+					V.NbDuplication();
+					break;
+				case 6 :
+					V.RemoveDuplication();
+					break;
+				case 7 : 
+					System.exit(0);
+				default :
+					System.out.println("Unknown operation");
+					break;
+			}
+		} while(choice != 3);
+		
+	}
+}
