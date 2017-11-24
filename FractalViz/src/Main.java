bd72ed1c9125df337416f2ba05d775955f7b02ab
NanoClem <root@Clment-PC.localdomain>
2017-11-24 22:43:24 +0100

    Test de bouttons et modif arborescenece

---
+++ FractalViz/src/Main.java
@@ -0,0 +1,17 @@
+// For compilation add "-classpath /Full_path/FractalViz/src"
+
+import packages.component.TestButton;
+import java.awt.*;
+
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    // Frame window = new Frame("FractalViz");
+    // window.setSize(400,300);
+    // window.setVisible(true);
+
+    TestButton window = new TestButton("Test button");
+  }
+}
---
+++ FractalViz/src/packages/component/TestButton.java
@@ -0,0 +1,35 @@
+// Do not forgot to make public function
+// if you want to use them outside of the package
+
+package packages.component;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class TestButton extends Frame
+                 implements ActionListener
+{
+  Button button;
+
+
+  public TestButton(String s)
+  {
+    super(s);
+    setSize(320,200);
+    setLayout(new FlowLayout() );
+
+    button = new Button("Send_nudes");
+    button.setActionCommand("send nudes button");
+    button.addActionListener(this);
+    add(button);
+
+    setVisible(true);
+  }
+
+
+  public void actionPerformed(ActionEvent event)
+  {
+    if(event.getActionCommand().equals("send nudes button") )
+      System.out.println("You're about to send some cool nudes");
+  }
+}
