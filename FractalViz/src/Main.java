17374e2d6754368033fb42c4374e0a823614ac7a
NanoClem <root@Clment-PC.localdomain>
2017-11-26 00:04:24 +0100

    refresh

--- FractalViz/src/Main.java
+++ FractalViz/src/Main.java
@@ -10,13 +10,13 @@ public class Main
   public static void main(String[] args)
   {
     JFrame window = new JFrame("FractalViz");
-    Container paneContent = window.getContentPane();
 
     window.setLocationRelativeTo(null);                         // put the window in center of the physical screen
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      // end the program on closing
     window.setResizable(false);                                 // resize the window is forbidden
     window.setSize(400,300);
 
+    // Container paneContent = window.getContentPane();
     // TestButton myButton = new TestButton("Test button");
     // paneContent.add(myButton);
 
