fbf00d9ce0fa08ced160637ab5244dd7b82ce5df
NanoClem <root@Clment-PC.localdomain>
2017-12-01 19:08:52 +0100

    Ajout du texte par défaut, sauver le fichier, finir la couleur du texte

--- TpJava/TP07/GraphicWindow.java
+++ TpJava/TP07/GraphicWindow.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -11,7 +12,7 @@ public class GraphicWindow extends JFrame
 {
   TextArea textPane;
 
-  public GraphicWindow(String s)
+  public GraphicWindow(String s) throws Exception
   {
     super(s);
     setSize(600,400);
@@ -52,49 +53,127 @@ public class GraphicWindow extends JFrame
     color.add(custom);
 
 
-    //Les actions lorsque l'on sélectionne les items
-    quit.setActionCommand("quit");      // lorsque l'on clique sur "Quitter"
+    // AJOUTS DES ACTION DE CLIC SUR LES ITEMS
+    quit.setActionCommand("quit");                       // lorsque l'on clique sur "Quitter"
     quit.addActionListener(this);
 
-    load.setActionCommand("load");      // lorsque l'on clique sur "Charger"
+    load.setActionCommand("load");                       // lorsque l'on clique sur "Charger"
     load.addActionListener(this);
+
+    save.setActionCommand("save");                       // Lorsque l'on clique sur "Sauver"
+    save.addActionListener(this);
+
+    black.setActionCommand("black_text");
+    black.addActionListener(this);
+
+    red.setActionCommand("red_text");
+    red.addActionListener(this);
+
+    blue.setActionCommand("blue_text");
+    blue.addActionListener(this);
+
 
 
     // LA FENÊTRE DE TEXTE
-    textPane = new TextArea();
-    getContentPane().add(textPane);
+    textPane = new TextArea();                            // la fenêtre de texte
+    getContentPane().add(textPane);                       // on l'ajoute avec son contenu dans notre fenêtre graphique
+
+    JScrollPane scrollBar = new JScrollPane(textPane);    // Barre de scrolling verticale
+    getContentPane().add(scrollBar);
 
 
     setVisible(true);
   }
 
 
+
   public void actionPerformed(ActionEvent event)
   {
+
     // ACTION LORSQUE L'ON CLIQUE SUR "Quitter"
-    if ( event.getActionCommand().equals("quit") )       // si on sélectionne "Quitter"
+    if ( event.getActionCommand().equals("quit") )                 // si on sélectionne "Quitter"
     {
       JOptionPane pane = new JOptionPane();
-      if ( pane.showConfirmDialog(this,                  // on demande une confirmation en rapport
-                "Voulez vous vraiment quitter ?",        // avec la fenêtre qui a déclenché l'action (this)
+      if ( pane.showConfirmDialog(this,                            // on demande une confirmation en rapport
+                "Voulez vous vraiment quitter ?",                  // avec la fenêtre qui a déclenché l'action (this)
                 "Attention",
                 pane.YES_NO_OPTION,
-                pane.WARNING_MESSAGE) == JOptionPane.YES_OPTION )
-        System.exit(0);
+                pane.WARNING_MESSAGE) == JOptionPane.YES_OPTION )  // si on a confirmé
+        System.exit(0);                                            // on quitte le programme
     }
+
 
     // ACTION LORSQUE L'ON CLIQUE SUR "Charger"
     if ( event.getActionCommand().equals("load") )
     {
-      JFileChooser select = new JFileChooser();                               // Sélecteur de fichier
+      JFileChooser select = new JFileChooser();                                                       // Sélecteur de fichier
       select.addChoosableFileFilter( new FileNameExtensionFilter("Text Documents(.txt)", "txt") );    // On ne peut sélectionner que les fichiers texte
-      int result = select.showOpenDialog(this);                               // On affiche une fenêtre de dialog
+      int result = select.showOpenDialog(this);                                                       // On affiche une fenêtre de dialog
 
-      if(result == JFileChooser.APPROVE_OPTION)                               // Si on a confirmé le chargement du fichier
+      if(result == JFileChooser.APPROVE_OPTION)                                                       // Si on a confirmé le chargement du fichier
       {
-        System.out.println("Fichier choisi = " + select.getSelectedFile().getAbsolutePath() );
-        // Remplacer par l'affichage du fichier dans le JTextPane
+        File choosed_file = new File( select.getSelectedFile().getAbsolutePath() );                   // le fichier choisi avec le chemin complet
+        try {
+          textPane.setPage( choosed_file.toURI().toURL() );                                           // on ajoute le contenu du fichier dans la fenêtre de texte
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+        }
       }
     }
+
+
+    // ACTION LORQUE L'ON CLIQUE SUR "Sauver"
+    if( event.getActionCommand().equals("save") )
+    {
+      JFileChooser save = new JFileChooser();
+      int result = save.showSaveDialog(this);                                   // Nouvelle fenêtre de sauvegarde
+
+      if(result == JFileChooser.APPROVE_OPTION)
+       {
+         File saved_file = new File( save.getSelectedFile().getAbsolutePath() );
+         String s = saved_file.getPath();                                       // Nom du fichier sauvé
+         int i = s.indexOf(".");                                                // index du caractère "." juste avant l'extension
+         String txt = textPane.getText();                                       // contenu de la fenêtre de texte
+         PrintWriter writer;
+
+         if ( i == -1 || !s.substring(i).equals(".txt") )                       // si il n'y a pas de "." en fin de nom ou si le nom n'est pas consittué de ".txt"
+         {
+           try
+           {
+             writer = new PrintWriter( new FileWriter(s + ".txt"), true );      // on écrit un nouveau fichier avec la bonne extension
+             writer.println(txt);                                               // on écrit le texte de la TextArea dans le fichier
+             writer.close();                                                    // fermeture du fichier
+           }
+           catch (IOException e)
+           {
+             e.printStackTrace();
+           }
+         }
+        else                                                                    // Si l'extension est bien ".txt"
+        {
+          try {
+            writer = new PrintWriter( new FileWriter(s), true );
+            writer.println(txt);
+            writer.close();
+          }
+          catch (IOException e) {
+            e.printStackTrace();
+          }
+       }
+     }
+   }
+
+   // METTRE LE TEXTE EN NOIR
+   if ( event.getActionCommand().equals("black") )
+     textPane.setTextStyle(Color.black);
+
+  // METTRE LE TEXTE EN ROUGE
+   if ( event.getActionCommand().equals("red") )
+     textPane.setTextStyle(Color.red);
+
+  // METTRE LE TEXTE EN BLEU
+   if ( event.getActionCommand().equals("blue") )
+     textPane.setTextStyle(Color.blue);
   }
 }
--- TpJava/TP07/TextArea.java
+++ TpJava/TP07/TextArea.java
@@ -1,12 +1,39 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.Style;
+import javax.swing.text.StyledDocument;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.SimpleAttributeSet;
+
 
 
 public class TextArea extends JTextPane
 {
 
-  public TextArea()
+
+  public TextArea() throws Exception
   {
-    
+    setTextStyle(Color.black);
+  }
+
+
+  public void setTextStyle(Color color)
+  {
+    // // STYLE DU textPane
+    StyledDocument doc = (StyledDocument) this.getDocument();
+    Style style = doc.addStyle("BasicText", null);                           // Objet de style
+    StyleConstants.setForeground(style, color);                        // Attributs du premier plan
+
+    //SimpleAttributeSet set = new SimpleAttributeSet();
+    //StyleConstants.setForeground(set, color);                             // Attributs du premier plan
+    //this.setCharacterAttributes(set, true);                               // Attributs du texte
+
+    try {
+      doc.insertString(doc.getLength(), "Texte par defaut", style);         // Insertion du texte de base
+    }
+    catch(Exception e) {
+      e.getMessage();
+      // /e.offSetRequested();                // retourne le déplacement demandé dans l'emplacement mémoire concerné
+    }
   }
 }
